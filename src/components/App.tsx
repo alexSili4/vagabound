90aac8680e7d6dac5be2cd1f5183eafb57cac405
Silich <sili41995@gmail.com>
2025-11-26 06:54:03 +0200

    add preview video

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { FC, lazy } from 'react';
 import { PagePaths } from '@/constants';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from '@CommonComponents/SharedLayout';
+import PreviewVideo from '@CommonComponents/PreviewVideo';
 
 const NotFoundPage = lazy(() => import('@/pages/NotFoundPage'));
 const MainPage = lazy(() => import('@/pages/MainPage'));
@@ -9,15 +10,17 @@ const MaisiePage = lazy(() => import('@/pages/MaisiePage'));
 
 const App: FC = () => {
   return (
-    // <MainPage />
-    <Routes>
-      <Route path={PagePaths.root} element={<SharedLayout />}>
-        <Route index element={<MainPage />} />
-        <Route path={PagePaths.root} element={<MainPage />} />
-        <Route path={PagePaths.maisie} element={<MaisiePage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <>
+      <PreviewVideo />
+      <Routes>
+        <Route path={PagePaths.root} element={<SharedLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path={PagePaths.root} element={<MainPage />} />
+          <Route path={PagePaths.maisie} element={<MaisiePage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
