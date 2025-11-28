c8347d815af72184c50141381a43f8cb55a744c1
Silich <sili41995@gmail.com>
2025-11-28 15:12:01 +0200

    upd footer

--- src/components/MainPage/Main/Main.tsx
+++ src/components/MainPage/Main/Main.tsx
@@ -1,30 +1,30 @@
 import { FC } from 'react';
-import HeroSection from '@MainPageComponents/HeroSection';
-import AboutSection from '@MainPageComponents/AboutSection';
-import BarrelsSection from '@MainPageComponents/BarrelsSection';
-import TravelSection from '@MainPageComponents/TravelSection';
-import StorySection from '@MainPageComponents/StorySection';
-import MediaSection from '@MainPageComponents/MediaSection';
-import BottlesSection from '@MainPageComponents/BottlesSection';
-import OrderSection from '@MainPageComponents/OrderSection';
-import FAQSection from '@MainPageComponents/FAQSection';
-import BagSection from '@MainPageComponents/BagSection';
+// import HeroSection from '@MainPageComponents/HeroSection';
+// import AboutSection from '@MainPageComponents/AboutSection';
+// import BarrelsSection from '@MainPageComponents/BarrelsSection';
+// import TravelSection from '@MainPageComponents/TravelSection';
+// import StorySection from '@MainPageComponents/StorySection';
+// import MediaSection from '@MainPageComponents/MediaSection';
+// import BottlesSection from '@MainPageComponents/BottlesSection';
+// import OrderSection from '@MainPageComponents/OrderSection';
+// import FAQSection from '@MainPageComponents/FAQSection';
+// import BagSection from '@MainPageComponents/BagSection';
 
 const Main: FC = () => {
   return (
     <>
       <div style={{ overflow: 'hidden' }}>
-        <HeroSection />
-        <BarrelsSection />
+        {/* <HeroSection /> */}
+        {/* <BarrelsSection /> */}
       </div>
-      <AboutSection />
-      <TravelSection />
-      <StorySection />
-      <MediaSection />
-      <BottlesSection />
-      <OrderSection />
-      <FAQSection />
-      <BagSection />
+      {/* <AboutSection /> */}
+      {/* <TravelSection /> */}
+      {/* <StorySection /> */}
+      {/* <MediaSection /> */}
+      {/* <BottlesSection /> */}
+      {/* <OrderSection /> */}
+      {/* <FAQSection /> */}
+      {/* <BagSection /> */}
     </>
   );
 };
